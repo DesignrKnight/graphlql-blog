3da5894c293aca2c8a858a2a6a704e63f6de7a15
DesignrKnight <notify.abelmathew@gmail.com>
2020-08-27 13:32:43 +0530

    Add root endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app. use('/',(req,res) => {
+    res.send("Welcome to GraphQL server. Use GraphQL endpoint at /graphql")
+})
+
 app.listen(5000, () => {
     console.log('now listening for requests on port 5000');
 });
