0806adf242d811cacb8f68e8a2c725621e68ebcd
DesignrKnight <notify.abelmathew@gmail.com>
2020-08-27 14:25:31 +0530

    GraphQL Init

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+
 
 const app = express();
 
-app. use('/',(req,res) => {
-    res.send("Welcome to GraphQL server. Use GraphQL endpoint at /graphql")
-})
+app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      graphiql: true,
+    }));  
 
 app.listen(5000, () => {
     console.log('now listening for requests on port 5000');
